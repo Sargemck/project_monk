7195564ed33b81bf90feefda2fa78c9c3e24c782
DESKTOP-QGAMGVK\baris <baris.mckenna7@mail.dcu.ie>
2017-12-28 04:18:09 +0000

    Added raycasting

--- Assets/Assets/Scripts/CameraFollow.cs
+++ Assets/Assets/Scripts/CameraFollow.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
 
     GameObject player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void LateUpdate(){
+    private void LateUpdate()
+    {
         transform.position = player.transform.position;
     }
 }

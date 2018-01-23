d29924e9e40dc451e38c2289163dfa51daccc61e
DESKTOP-QGAMGVK\baris <baris.mckenna7@mail.dcu.ie>
2018-01-23 13:16:19 +0000

    Pre-SSD Messing

---
+++ Assets/Assets/Camera Stuff/CursorAffordance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D moveCursor = null;
+    [SerializeField] Texture2D fightCursor = null;
+    [SerializeField] Texture2D nopeCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(64, 64);
+
+    Texture2D pickedCursor = null;
+    CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                pickedCursor = moveCursor;
+                break;
+            case Layer.RaycastEndStop:
+                pickedCursor = nopeCursor;
+                break;
+            case Layer.Enemy:
+                pickedCursor = fightCursor;
+                break;
+            default:
+                Debug.LogError("I dun goofed... unknown layer hit");
+                return;
+        }
+        Cursor.SetCursor(pickedCursor, cursorHotspot, CursorMode.Auto);
+     //   print (cameraRaycaster.layerHit);
+	}
+}
---
+++ Assets/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    //TODO fix affordance being too large?
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        ProcessMouseclickMovement();
+        
+    }
+
+    private void ProcessMouseclickMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit layer: " + cameraRaycaster.layerHit);
+            switch (cameraRaycaster.layerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    break;
+                case Layer.Enemy:
+                    print("This is an enemy. Not walking towards");
+                    break;
+                default:
+                    print("Unexpected layer hit");
+                    return;
+            }
+        }
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+            m_Character.Move(playerToClickPoint, false, false);
+        }
+        else
+        {
+            m_Character.Move(Vector3.zero, false, false);
+        }
+    }
+}
+
